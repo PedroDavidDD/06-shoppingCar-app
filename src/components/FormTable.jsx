3c0e4fc9e5153cf6f1660e1f04ec16a5f521ef9e
AvariciaT <83774625+AvariciaT@users.noreply.github.com>
2022-12-24 16:06:17 -0500

    Diseño terminado-products-table

--- src/components/FormTable.jsx
+++ src/components/FormTable.jsx
@@ -4,6 +4,26 @@ export const FormTable = () => {
   return (
     <>
         <h1 className='bg-red-500'>FormTable</h1>
+        <div className="table-responsive">
+            <table className="table table-hover">
+                <thead>
+                    <tr className='table-dark'>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody className='table-warning'>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </>
     )
 }
